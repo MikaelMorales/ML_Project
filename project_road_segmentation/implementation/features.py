91b19fe00b8a9dfc3dcbc7378435f82156311625
caizan <vincent.petri@epfl.ch>
2017-11-18 16:08:46 +0100

    first try with patch indexes

--- project_road_segmentation/implementation/features.py
+++ project_road_segmentation/implementation/features.py
@@ -16,9 +16,27 @@ def extract_features_2d(img):
     feat = np.append(feat_m, feat_v)
     return feat
 
+# Extracr 8-dimensional features consisting of average RGB color as well as variance and indexes
+def extract_features_8d(img,idx,patch_size, length):
+    feat = extract_features(img)
+    n = length / patch_size
+    feat_i = idx / n
+    feat = np.append(feat, feat_i)
+    feat_j = idx % n
+    feat = np.append(feat,feat_j)
+    return feat
+
 # Extract features for a given image
 def extract_img_features(filename, patchSize):
     img = load_image(filename)
     img_patches = img_crop(img, patchSize, patchSize)
     X = np.asarray([extract_features_2d(img_patches[i]) for i in range(len(img_patches))])
+    return X
+
+
+# Extract features for a given image
+def extract_img_features_8d(filename, patchSize):
+    img = load_image(filename)
+    img_patches = img_crop(img, patchSize, patchSize)
+    X = np.asarray([extract_features_8d(img_patches[i],i,patchSize,400) for i in range(len(img_patches))])
     return X
